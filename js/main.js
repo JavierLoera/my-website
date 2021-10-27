83363abc66809c509d42dec49382546fe3520e75
JavierLoera <jesus_loera_15@hotmail.com>
2021-10-27 17:16:07 -0500

    subir cv

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const header = document.getElementById(`header`);
+const toggle = document.getElementById(`toggle`);
+const navbar = document.getElementById(`navbar`);
+const icon = document.getElementById(`icon`);
+const intro = document.querySelector(".intro");
+
+//intro
+document.addEventListener("DOMContentLoaded", (e) => {
+	setTimeout(() => {
+		intro.classList.add("display-none");
+	}, 4000);
+});
+
+//menu
+document.onclick = function (e) {
+	if (
+		e.target.id != `header` &&
+		e.target.id != `toggle` &&
+		e.target.id != `navbar`
+	) {
+		toggle.classList.remove(`active`);
+		navbar.classList.remove(`active`);
+	}
+};
+icon.onclick = function (e) {
+	e.stopPropagation();
+	toggle.classList.toggle(`active`);
+	navbar.classList.toggle(`active`);
+};
+
+/*scroll locomotive*/
+const scroll = new LocomotiveScroll({
+	el: document.querySelector("[data-scroll-container]"),
+	smooth: true,
+	tablet: { smooth: true },
+	smartphone: { smooth: true },
+});
+
+//cursor
+
+let cursor = document.getElementById("cursor");
+
+if (innerWidth > 760) {
+	document
+		.getElementsByTagName("body")[0]
+		.addEventListener("mousemove", function (e) {
+			(cursor.style.left = e.clientX + "px"),
+				(cursor.style.top = e.clientY + "px");
+		});
+
+	function addHover(t) {
+		cursor.classList.add("hover");
+	}
+	function removeHover(t) {
+		cursor.classList.remove("hover");
+	}
+
+	removeHover();
+
+	function hoverOutTarget(e) {
+		e.addEventListener("mouseover", addHover),
+			e.addEventListener("mouseout", removeHover);
+	}
+
+	for (
+		let i = document.querySelectorAll(".hover-target"), a = i.length - 1;
+		a >= 0;
+		a--
+	) {
+		hoverOutTarget(i[a]);
+	}
+}
